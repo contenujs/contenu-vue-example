48006b0c3a6184ab0d21e8d84c32547d38ac2d5e
Nariman Movaffaghi <nariman.movaffaghi@gmail.com>
2020-08-27 16:14:09 +0430

    Import vue-contenu lib

--- src/main.js
+++ src/main.js
@@ -2,13 +2,27 @@
 import Vue from "vue";
 import App from "./App.vue";
 require("./assets/app.css");
-import Contenu from "./contenu";
-
+import Contenu from "vue-contenu";
+import VueRouter from "vue-router";
 Vue.config.productionTip = false;
+Vue.use(VueRouter);
+
+const routes = [
+  { path: "/", component: require("./index.vue").default },
+  { path: "/about", component: require("./about.vue").default }
+];
+
+const router = new VueRouter({
+  mode: "history",
+  routes
+});
+
 Vue.use(Contenu, {
   serverAddress: "http://localhost:8080",
-  fetchDataAddress: "/api/data"
+  fetchDataAddress: "/api/data",
+  router
 });
 window.app = new Vue({
-  render: h => h(App)
+  render: h => h(App),
+  router
 }).$mount("#app");
